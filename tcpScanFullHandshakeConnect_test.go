5476dd1e98b2fe506384708febe4f80d77797c4e
root <38677707+wyzerg@users.noreply.github.com>
2023-12-07 12:49:07 +0800

    move

--- tcpScanFullHandshakeConnect_test.go
+++ tcpScanFullHandshakeConnect_test.go
@@ -2,12 +2,11 @@ package tcpScanFullHandshakeConnect
 
 import (
 	"fmt"
-	"tcpScanFullHandshakeConnect/tcpScanFullHandshakeConnect"
 	"testing"
 )
 
 func Test_TcpScanFullHandshakeConnect(t *testing.T) {
-	result, err := tcpScanFullHandshakeConnect.TcpScanFullHandshakeConnect("127.0.0.1", "1000-8000", 5000)
+	result, err := TcpScanFullHandshakeConnect("127.0.0.1", "1000-8000", 5000)
 	if err != nil {
 		return
 	}
